5de074522e14861ce01c6815b39c47bf1c8557a0
Mehmet Durmaz <mehmetdurmazz@yahoo.com>
2020-02-25 23:38:22 +0300

    Added PetTypeService classes.

---
+++ pet-clinic-data/src/main/java/com/springframeworktraining/sfpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.springframeworktraining.sfpetclinic.services.map;
+
+import com.springframeworktraining.sfpetclinic.model.PetType;
+import com.springframeworktraining.sfpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
